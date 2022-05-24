6f5ba7bce0739c7ba0cb3a697a06ea14fd1722dd
Himangshu1086 <baishyahimangshu499@gmail.com>
2022-05-24 16:07:08 +0700

    update

--- client/src/COMPONENTS/ABOUT/About.js
+++ client/src/COMPONENTS/ABOUT/About.js
@@ -1,12 +1,111 @@
 import React from 'react'
-
+import '../../styles/About.css'
+import logo from '../../ASSETS/logo.jpg'
 function About() {
   return (
-    <main>
-      <div className="sakti-container">
-        <div>
-          <h1>Coming Soon</h1>
-        </div>
+    <main style={{background:"linear-gradient(to right, #0f2027, #203a43, #2c5364)"}} >
+
+<div style={{background:"black"}}><img style={{width:"100%",height:"400px",opacity:"0.4"}} 
+src="https://images.unsplash.com/photo-1455849318743-b2233052fcff?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169" />
+    </div>
+    <h1 style={{color:"white",top:"5%",left:"15%",marginTop:"3%",textAlign:"center",fontSize:"4rem",color:"white"}}>ABOUT AASHRAYA</h1>
+    <div style={{padding:"8%"}}>
+    
+    <div style={{marginBottom:"7%",marginTop:"-5%"}}>
+      <p style={{color:"white", textAlign:"justify" , margin:"auto",width:"85%"}}>
+      One in every ten children is disabled in India. 
+      Only 38% of India's children below the age of 2 years are immunised. 
+      At least 35 million children aged 6 - 14 years do not attend school. 
+      Other millions of children who dream of schooling and a better livelihood are just left uncared for. 
+      On the other hand, those deprived women and other individuals of society struggle each day to make a living. <br/>
+      Transforming these numbers for the general good is what drives us each morning. With an aim to provide holistic development 
+      to society via a combination of different actions, Team Aashraya was set up in 2019 to work with grassroots initiatives to 
+      effect positive changes in the lives of the nation's underprivileged community and our mother nature as a whole. <br/>
+      We aim to work as a catalyst in providing sustainable changes in the lives left unseen and ignored and deploy the best 
+      possible methodology for achieving success. 
+We believe in multidisciplinary development and focus on 
+working in different sectors be it environment, women and children empowerment, healthcare, proper clothing, and education. <br/>
+Our motive is clear- ensure proactive participation of all for the general good. We may not be of much help when working individually,
+ but we can do so much once together.
+
+
+      </p>
+    </div>
+
+    <div style={{position:"relative",display:"flex",flexWrap:"wrap",justifyContent:"center",margin:"7% 0%"}}>
+      
+      <div class="card" style={{width: "45%",background:"linear-gradient(to right, #00f260, #0575e6)",height:"auto",boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",border :"none",margin:"20px",padding:"40px"}}>
+        <h1 style={{fontSize:"4rem",color:"white",textAlign:"center"}}>OUR MOTO</h1>
+        <h3 style={{textAlign:"center",color:"white"}}>TAMASO MA<br/> JYOTIRGAMAYA</h3>
+    </div>
+    <div class="card" style={{width: "45%",background:"linear-gradient(to right, #00f260, #0575e6)",height:"auto",boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",border :"none",margin:"20px",padding:"40px"}}>
+        <h1 style={{fontSize:"4rem",color:"white",textAlign:"center"}}>OUR VISION</h1>
+        <h3 style={{textAlign:"center",color:"white"}}>SVASMAI SVALPAṂ <br/>SAMĀJĀYA SARVASVAṂ।</h3>
+    </div>
+
+      </div>
+
+      <h1 style={{textAlign:"center",color:"white",fontSize:"4rem",margin:"40px"}}>OUR GOALS</h1>
+      <div style={{position:"relative",display:"flex",flexWrap:"wrap",justifyContent:"center"}}>
+      
+      <div class="card" style={{width: "26rem",height:"26rem",boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",border :"none",margin:"20px"}}>
+        <h1 style={{position:"absolute",color:"white",top:"25%",left:"20%",fontSize:"4rem",color:"white"}}>Shiksha</h1>
+      <div style={{background:"black"}}>
+          <img class="card-img-top" style={{opacity:"0.4",borderRadius:"0",margin:"0"}} src="https://images.unsplash.com/photo-1523580846011-d3a5bc25702b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070" alt="Card image cap"/>
+      </div>
+      <div class="card-body">
+        <p class="card-text">In Shiksha, we are devoted to making education accessible to all, especially the underprivileged. Currently, we are committed to our Spread Knowledge Campaign.</p>
+      </div>
+      </div>
+
+      <div class="card" style={{width: "26rem",height:"26rem",boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",border :"none",margin:"20px"}}>
+        <h1 style={{position:"absolute",color:"white",top:"25%",left:"20%",fontSize:"4rem",color:"white"}}>Chikitsa</h1>
+      <div style={{background:"black"}}>
+          <img class="card-img-top" style={{opacity:"0.4",borderRadius:"0",margin:"0"}} 
+          src="https://images.unsplash.com/photo-1624727828489-a1e03b79bba8?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171" alt="Card image cap"/>
+      </div>
+      <div class="card-body">
+        <p class="card-text">
+        We aim to make the health facilities and resources easily accessible to all, especially those who need help in understanding and utilising those.
+        </p>
+      </div>
+      </div>
+
+
+      <div class="card" style={{width: "26rem",height:"26rem",boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",border :"none",margin:"20px"}}>
+        <h1 style={{position:"absolute",color:"white",top:"25%",left:"15%",fontSize:"4rem",color:"white"}}>Paryavaran</h1>
+      <div style={{background:"black"}}>
+          <img class="card-img-top" style={{opacity:"0.4",borderRadius:"0",margin:"0"}}
+           src="https://images.unsplash.com/photo-1421789665209-c9b2a435e3dc?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171" 
+           alt="Card image cap"/>
+      </div>
+      <div class="card-body">
+        <p class="card-text">
+        Through Paryavaran we aim to contribute towards nature through plantation, cleanliness drives, etc
+
+        </p>
+      </div>
+      </div>
+
+
+      <div class="card" style={{width: "26rem",height:"26rem",boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",border :"none",margin:"20px"}}>
+        <h1 style={{position:"absolute",color:"white",top:"25%",left:"20%",fontSize:"4rem",color:"white"}}>Vastra</h1>
+      <div style={{background:"black"}}>
+          <img class="card-img-top" style={{opacity:"0.4",borderRadius:"0",margin:"0"}} 
+          src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"
+           alt="Card image cap"/>
+      </div>
+      <div class="card-body">
+        <p class="card-text">
+        Through Vastra we aim to fulfil the need for basic amenities of our fellow living beings. We are organising cloth donation drives, food donation drives, dog coats, etc. in different regions.
+
+        </p>
+      </div>
+      </div>
+
+
+
+      </div>
       </div>
     </main>
   )
